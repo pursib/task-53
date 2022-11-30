9d74cfabe82df02f1eb0fa37c57c39562bea5fe4
Preslav <pursib@abv.bg>
2022-11-30 17:36:00 +0200

    Adding attributes dynamically
    Create a JS implementation which add "data-price" attribute to every component with attribute 'product'

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,10 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-
+let products = document.querySelectorAll('.product');
+products.forEach(product => {
+  product.classList.add('data-price')
+  let data_price = document.querySelector('.price').textContent;
+  console.log(data_price);
+});
 });
